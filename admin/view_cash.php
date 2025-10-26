613de48fe7d523e053c9c192fa65e75cfc9db778
darkdragon630 <71604929+darkdragon630@users.noreply.github.com>
2025-10-26 08:17:13 +0700

    Fix comment syntax in view_cash.php

--- admin/view_cash.php
+++ admin/view_cash.php
@@ -5,7 +5,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-/ DEBUG: Tampilkan semua parameter
+// DEBUG: Tampilkan semua parameter
 echo "<pre>";
 echo "GET params: ";
 print_r($_GET);
